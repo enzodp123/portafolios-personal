895d4aab48843fbfa20b1dbcbe92c2c08de9f4de
enzo <enzopazwork@gmail.com>
2026-01-04 08:46:34 -0300

    create portfolio

---
+++ app.js
@@ -0,0 +1,9 @@
+  tailwind.config = {
+        theme: {
+          extend: {
+            boxShadow: {
+              soft: "0 20px 50px -20px rgba(0,0,0,.35)",
+            },
+          },
+        },
+      };
